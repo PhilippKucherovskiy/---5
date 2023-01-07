0b4340c0dcd8476bfe7902be235c5d5418f8b2c4
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-01-07 12:59:59 +0300

    Добавьте файлы проекта.

---
+++ Итоговый пр-т Модуля 5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Итоговый_пр_т_Модуля_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
